61294f4406f93ceda7f3f2be3f4f2172e789e52f
Lime <alsayyd.medo72@gmail.com>
2024-02-28 23:50:55 +0200

    first commit

---
+++ src/index.js
@@ -0,0 +1,43 @@
+import express from "express";
+import dotenv from "dotenv";
+dotenv.config();
+import mongoose from "mongoose";
+const app = express();
+import cors from "cors";
+import swaggerUi from "swagger-ui-express";
+import { SwaggerTheme } from "swagger-themes";
+import fs from "fs";
+
+const outputFile = JSON.parse(fs.readFileSync("./swagger_output.json"));
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+const theme = new SwaggerTheme();
+app.use(
+  "/api-docs",
+  swaggerUi.serve,
+  swaggerUi.setup(outputFile, {
+    explorer: true,
+    customCss: theme.getBuffer("dark"),
+  })
+);
+
+app.get("/", (req, res) => {
+  return res.send("Hello World!");
+});
+
+const start = () => {
+  mongoose
+    .connect(process.env.MONGO_URI)
+    .then(() => {
+      app.listen(process.env.PORT, () => {
+        console.log(
+          `listening on port http://localhost:${process.env.PORT} connected to db`
+        );
+      });
+    })
+    .catch((err) => console.log(err));
+};
+
+start();
